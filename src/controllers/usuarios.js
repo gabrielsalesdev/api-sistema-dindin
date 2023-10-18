1c17889747cf9d55c3da0bbcee2d40489d09102b
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-10-18 11:12:27 -0300

    feat: implementando o controlador de atualizar o usuário

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -33,4 +33,24 @@ const detalharUsuario = async (req, res, next) => {
     }
 };
 
+const atualizarUsuario = async (req, res, next) => {
+    try {
+        const id = req.id;
+        const { nome, email, senha } = req.body;
+
+        if (!nome || !email || !senha) throw new HttpError('Dados obrigatórios não fornecidos', 400);
+
+        const { rowCount } = await pool.query('SELECT * FROM usuarios WHERE email = $1 AND id != $2;', [email, id]);
+        if (rowCount > 0) throw new HttpError('O e-mail fornecido já está sendo utilizado por outro usuário', 409);
+
+        const senhaCrypt = await bcrypt.hash(senha, 10);
+
+        await pool.query('UPDATE usuarios SET nome = $1, email = $2, senha = $3 WHERE id = $4', [nome, email, senhaCrypt, id]);
+
+        return res.status(204).end();
+    } catch (e) {
+        next(e);
+    }
+};
+
 module.exports = { cadastrarUsuario, detalharUsuario, atualizarUsuario };
